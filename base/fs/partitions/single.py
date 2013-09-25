1940e7802c32455f22db06c444a4139ca52c3819
Anders Ingemann <anders@ingemann.de>
2013-09-25 23:07:19 +0200

    Simplify FSM framework. Only model relevant states

--- base/fs/partitions/single.py
+++ base/fs/partitions/single.py
@@ -3,5 +3,5 @@ from abstract import AbstractPartition
 
 class SinglePartition(AbstractPartition):
 
-	def _create(self, e):
+	def _before_create(self, e):
 		self.device_path = e.volume.device_path
